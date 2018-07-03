84381429605d8b97e01b54725f77fa3635eac2a3
KMishik <k.mishik@gmail.com>
2018-07-03 17:14:56 +0500

    Добавлена страница 'Конкурс на научные вакансии'

---
+++ src/tsc/ufic-competition.ts
@@ -0,0 +1,34 @@
+!function($){
+
+  $(document).ready(function(){
+    $('.subdiv').on('click', subdivOnClick);
+    $('.month').on('click', monthOnClick);
+  })
+
+  function subdivOnClick(event: Event){
+    let root: String = '.competition';
+    if ($(event.target).attr('class') === 'subdiv__title') {
+      if($(".subdiv__date", this).hasClass('list_hidden')) {
+        $(".list", root).addClass('list_hidden');
+        $(".subdiv__date", this).removeClass('list_hidden')
+      } else {
+        $(".list", this).addClass('list_hidden')
+      }
+      event.stopPropagation();
+    }
+  }
+
+  function monthOnClick(event: Event) {
+    let root: String = '.competition';
+    if ($(event.target).attr('class') === 'month__title') {
+      if ($('.list', this).hasClass('list_hidden')) {
+        $('.month__doclinks', root).addClass('list_hidden');
+        $(".month__doclinks", this).removeClass('list_hidden');
+      } else {
+        $(".month__doclinks", this).addClass('list_hidden');
+      }
+      event.stopPropagation();
+    }
+  }
+
+}(jQuery);
